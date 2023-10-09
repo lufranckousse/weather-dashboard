c5e0142df349a9c03bd07e013b78113f489fdea0
lufranc Kousse <kousselufranc477@gmail.com>
2023-10-08 22:16:43 -0500

    added, html, css and js

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,271 +1,103 @@
-const API_KEY = "81274b6b41cb15dc0dd390c01fc36c70"
+const cityInput = document.querySelector(".city-input");
+const searchButton = document.querySelector(".search-btn");
+const locationButton = document.querySelector(".location-btn");
+const currentWeatherDiv = document.querySelector(".current-weather");
+const weatherCardsDiv = document.querySelector(".weather-cards");
 
-//calls the fetchWeatherAPI function and clears the search input
-function search(){
-    var searchInput = $("#search-input").val();
-    fetchWeatherAPI(searchInput);
-    $("#search-input").val("");
+const API_KEY = "81274b6b41cb15dc0dd390c01fc36c70"; // API key for OpenWeatherMap API
+
+const createWeatherCard = (cityName, weatherItem, index) => {
+    if(index === 0) { // HTML for the main weather card
+        return `<div class="details">
+                    <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+                    <h6>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+                    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+                    <h6>Humidity: ${weatherItem.main.humidity}%</h6>
+                </div>
+                <div class="icon">
+                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+                    <h6>${weatherItem.weather[0].description}</h6>
+                </div>`;
+    } else { // HTML for the other five day forecast card
+        return `<li class="card">
+                    <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+                    <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+                    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+                    <h6>Humidity: ${weatherItem.main.humidity}%</h6>
+                </li>`;
+    }
 }
 
-//crates a fetch for weather, returning alerts if there are errors, returning API data if status ok, calls functions to display data from API
-function fetchWeatherAPI(input){
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=" + API_KEY +"&units=imperial";
-    fetch(queryURL)
-        .then(function(response){
-            if(response.ok){
-                return response.json();
-            }else if(response.status == 400){
-                alert("HTTP-Error: " + response.status + "\nPlease enter a valid city name in the Search bar.");
-            }else if(response.status == 404){
-                alert("HTTP-Error: " + response.status + "\nThe city name you entered is not valid.\nPlease enter a valid city name in the Search bar.");
-            }else{
-                alert("HTTP-Error: " + response.status);
+const getWeatherDetails = (cityName, latitude, longitude) => {
+    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+
+    fetch(WEATHER_API_URL).then(response => response.json()).then(data => {
+        // Filter the forecasts to get only one forecast per day
+        const uniqueForecastDays = [];
+        const fiveDaysForecast = data.list.filter(forecast => {
+            const forecastDate = new Date(forecast.dt_txt).getDate();
+            if (!uniqueForecastDays.includes(forecastDate)) {
+                return uniqueForecastDays.push(forecastDate);
             }
-            
-        })
-        .then(function(data){
-            var name = data.name;
-            var testKey = "HDF-weatherApp-" + name;
-            if(!(localStorage.getItem(testKey))){
-                addToList(name);
-            }
-            fetchForecastAPI(name);
-            displayToday(data);
-            
         });
-}
-//creates a fetch for 5day forecast, returns alerts if there are errors, returning API data if status ok, calls functions to display 5-day forecast
-function fetchForecastAPI(input){
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=" + API_KEY +"&units=imperial&";
-    fetch(queryURL)
-        .then(function(response){
-            if(response.ok){
-                return response.json();
-            }else if(response.status == 400){
-                alert("HTTP-Error: " + response.status + "\nPlease enter a valid city name in the Search bar.");
-            }else if(response.status == 404){
-                alert("HTTP-Error: " + response.status + "\nThe city name you entered is not valid.\nPlease enter a valid city name in the Search bar.");
-            }else{
-                alert("HTTP-Error: " + response.status);
+
+        // Clearing previous weather data
+        cityInput.value = "";
+        currentWeatherDiv.innerHTML = "";
+        weatherCardsDiv.innerHTML = "";
+
+        // Creating weather cards and adding them to the DOM
+        fiveDaysForecast.forEach((weatherItem, index) => {
+            const html = createWeatherCard(cityName, weatherItem, index);
+            if (index === 0) {
+                currentWeatherDiv.insertAdjacentHTML("beforeend", html);
+            } else {
+                weatherCardsDiv.insertAdjacentHTML("beforeend", html);
             }
-            
-        })
-        .then(function(data){
-            display_5day(data);
-        });
-    
-}
-
-//builds an array of forecast objects that can used to display the average 5-day forecast weather info
-function buildForecast(city){
-    //API returns 40 forecasts, 3 hour increments.
-    console.log(city); 
-    var API_forecastList = city.list;
-    var today = dayjs().format("MM/DD/YY");
-    console.log("today",today)
-    var forecasts = [];
-    var temp_high = -1000;
-    var temp_low = 1000;
-    var prevDate = today;
-    var curDate = today;
-    var avg_windSpeed = 0;
-    var avg_humidity = 0;
-    var dataPts = 0;
-    var iconNum = "";
-    var iconsUsed = {
-        "clearSky": 0,
-        "fewClouds": 0,
-        "scatteredClouds": 0,
-        "brokenClouds": 0,
-        "showerRain": 0,
-        "rain": 0,
-        "thunderstorm": 0,
-        "snow": 0,
-        "mist": 0,
-    }
-   //get date from each object
-   for(i = 0; i < API_forecastList.length; i++){
-    
-    //Next split index by "-" -> "YYYY", "MM", "DD"
-    curDate = (city.list[i].dt_txt.split(" ", 1))[0];
-    curDay = curDate.split("-", 3)[2];
-    curDate = curDate.split("-", 3)[1] + "/" + curDay + "/" + curDate.split("-", 3)[0];
-    if(i != 0){
-        prevDate = (city.list[i-1].dt_txt.split(" ", 1))[0];
-        prevDay = prevDate.split("-", 3)[2];
-        prevDate = prevDate.split("-", 3)[1] + "/" + prevDay + "/" + prevDate.split("-", 3)[0];
-    }else{
-        prevDate = curDate;
-        prevDay = curDay;
-    }
-
-    //if the date is today, then we move on to the next iteration of the loop
-    if(curDate == today){
-        continue;
-    }
-
-    //if the dates have transitioned to the next day, save the previous date's data and reset
-    if((curDate != prevDate) && (prevDate != today)){
-        iconNum = mostIcon(iconsUsed);
-        var newForecast = {
-            date: prevDate,
-            highTemp: temp_high,
-            lowTemp: temp_low,
-            windSpeed: (avg_windSpeed / dataPts).toFixed(2), //find the mean wind speed to 2 decimal places
-            humidity: (avg_humidity / dataPts).toFixed(0), //finds the mean humidity
-            icon: iconNum + "d",
-            
-        }
-        console.log("newForecast", newForecast);
-        console.log("iconNum", iconNum);
-        forecasts.push(newForecast);
-        temp_high = -1000;
-        temp_low = 1000;
-        avg_windSpeed = 0;
-        avg_humidity = 0;
-        dataPts = 0;
-        iconsUsed = {
-            "clearSky": 0,
-            "fewClouds": 0,
-            "scatteredClouds": 0,
-            "brokenClouds": 0,
-            "showerRain": 0,
-            "rain": 0,
-            "thunderstorm": 0,
-            "snow": 0,
-            "mist": 0,
-        }
-    }
-
-    if(temp_high < city.list[i].main.temp_max){
-        temp_high = city.list[i].main.temp_max;
-    }
-    if(temp_low > city.list[i].main.temp_min){
-        temp_low = city.list[i].main.temp_min;
-    }
-    avg_windSpeed += city.list[i].wind.speed; //adding up all wind speeds of current day
-    avg_humidity += city.list[i].main.humidity; //adding up all humidity
-    dataPts++; //getting data points for current day to later divide by
-
-    var icon = (city.list[i].weather[0].icon).substring(0,2);
-    switch(icon){
-        case "01": iconsUsed.clearSky += 1; break;
-        case "02": iconsUsed.fewClouds += 1; break;
-        case "03": iconsUsed.scatteredClouds += 1; break;
-        case "04": iconsUsed.brokenClouds += 1; break;
-        case "09": iconsUsed.showerRain += 1; break;
-        case "10": iconsUsed.rain += 1; break;
-        case "11": iconsUsed.thunderstorm += 1; break;
-        case "13": iconsUsed.snow += 1; break;
-        case "50": iconsUsed.mist += 1; break;
-        default: console.log("failure", icon);
-        
-    }
-    
-}
-    
-    var lastForecast = {
-        date: curDate,
-        highTemp: temp_high,
-        lowTemp: temp_low,
-        windSpeed: (avg_windSpeed / dataPts).toFixed(2), //find the mean wind speed to 2 decimal places
-        humidity: (avg_humidity / dataPts).toFixed(0), //finds the mean humidity
-    }
-    forecasts.push(lastForecast);
-
-    return forecasts;
-
-    
-
-}
-
-//returns id using name of icon
-function mostIcon(iconList){
-    const iconID ={
-        "clearSky": "01",
-        "fewClouds": "02",
-        "scatteredClouds": "03",
-        "brokenClouds": "04",
-        "showerRain": "09",
-        "rain": "10",
-        "thunderstorm": "11",
-        "snow": "13",
-        "mist": "50",
-    }
-    var mostName = "";
-    var mostNum = -1;
-    
-    for (icon in iconList) {
-        if(iconList[icon] > mostNum){
-            mostNum = iconList[icon];
-            mostName = icon;
-        }
-    }
-    
-    switch(mostName){
-        case "clearSky": return iconID.clearSky;
-        case "fewClouds": return iconID.fewClouds;
-        case "scatteredClouds": return iconID.scatteredClouds;
-        case "brokenClouds": return iconID.brokenClouds;
-        case "showerRain": return iconID.showerRain;
-        case "rain": return iconID.rain;
-        case "thunderstorm": return iconID.thunderstorm;
-        case "snow": return iconID.snow;
-        case "mist": return iconID.mist;
-        default: console.log("ERROR");return iconID.clearSky;
-    }
-}
-
-//Displays the relevant information using today's date
-function displayToday(city){
-    var dateTest = dayjs.unix(city.dt).format("MM/DD/YY");
-    $("#today-city-name").text(city.name + " ("+ dateTest +") ");
-    $("#icon").attr("src","https://openweathermap.org/img/w/" + city.weather[0].icon + ".png")
-    $("#today-city-temp").text("Temperature: " + city.main.temp + "\u00B0F");
-    $("#today-city-wind").text("Wind: " + city.wind.speed + " MPH");
-    $("#today-city-humidity").text("Humidity: " + city.main.humidity + "%");
-}
-
-//adds a button for a city to the list. creates an item in local storage containing city name
-function addToList(city){
-    var key = "HDF-weatherApp-" + city;
-    localStorage.setItem(key, city);
-    $("#city-list").append("<button type=\"button\" class=\"saved-city\">" + city + "</button>");
-
-}
-
-//cycles through local storage and displays the buttons of previously searched cities on load time
-function displayList(){
-    Object.keys(localStorage).forEach((key) => {
-        if(key.includes("HDF-weatherApp-")){
-            var name = localStorage.getItem(key);
-            $("#city-list").append("<button type=\"button\" class=\"saved-city\">" + name + "</button>");
-        }
+        });        
+    }).catch(() => {
+        alert("An error occurred while fetching the weather forecast!");
     });
 }
 
-//displays 5 day forecast
-function display_5day(city){
-    var forecastList = buildForecast(city);
-    console.log(forecastList);
-    $(".forecast-day-container").show();
-
-    var i = 0;
-    $(".forecast-day-container").each(function(){
-        $(this).children(".current-forecast-date").text(forecastList[i].date);
-        $(this).children("img").attr("src","https://openweathermap.org/img/w/" + forecastList[i].icon + ".png");
-        $(this).children(".high").text("High: " + forecastList[i].highTemp + "\u00B0F");
-        $(this).children(".low").text("Low: " + forecastList[i].lowTemp + "\u00B0F");
-        $(this).children(".wind").text("Avg Wind: " + forecastList[i].windSpeed + " MPH");
-        $(this).children(".humidity").text("Avg Humidity: " + forecastList[i].humidity + "%");
-        i++;
-    })
+const getCityCoordinates = () => {
+    const cityName = cityInput.value.trim();
+    if (cityName === "") return;
+    const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+    
+    // Get entered city coordinates (latitude, longitude, and name) from the API response
+    fetch(API_URL).then(response => response.json()).then(data => {
+        if (!data.length) return alert(`No coordinates found for ${cityName}`);
+        const { lat, lon, name } = data[0];
+        getWeatherDetails(name, lat, lon);
+    }).catch(() => {
+        alert("An error occurred while fetching the coordinates!");
+    });
 }
 
-//BUTTONS
-$("#search-btn").click(search);
-$("#city-list").on("click", ".saved-city", function(){
-    fetchWeatherAPI($(this).text());
-})
+const getUserCoordinates = () => {
+    navigator.geolocation.getCurrentPosition(
+        position => {
+            const { latitude, longitude } = position.coords; // Get coordinates of user location
+            // Get city name from coordinates using reverse geocoding API
+            const API_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`;
+            fetch(API_URL).then(response => response.json()).then(data => {
+                const { name } = data[0];
+                getWeatherDetails(name, latitude, longitude);
+            }).catch(() => {
+                alert("An error occurred while fetching the city name!");
+            });
+        },
+        error => { // Show alert if user denied the location permission
+            if (error.code === error.PERMISSION_DENIED) {
+                alert("Geolocation request denied. Please reset location permission to grant access again.");
+            } else {
+                alert("Geolocation request error. Please reset location permission.");
+            }
+        });
+}
 
-displayList();
+locationButton.addEventListener("click", getUserCoordinates);
+searchButton.addEventListener("click", getCityCoordinates);
+cityInput.addEventListener("keyup", e => e.key === "Enter" && getCityCoordinates());
